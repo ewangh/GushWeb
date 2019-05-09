3bc6ec70d194f39b71ad2ec6461e27faae3a2e07
ewangh <e_wangh@qq.com>
2019-05-09 22:29:41 +0800

    js

---
+++ GushWeb/Scripts/stockData.js
@@ -0,0 +1,30 @@
+﻿function updateDataChange(getCodes,func,hz) {
+    var timer = setInterval(function () { getStockData(getCodes, func) }, hz);
+}
+
+function getStockData(getCodes, func) {
+    var codes = getCodes();
+    $.ajax({
+        dataType: 'script',
+        async: true,
+        url: 'http://hq.sinajs.cn/list=' + getStockStr(codes),
+        cache: true,
+        success: function (msg) {
+            codes.forEach(function(item, index) {
+                var elements = eval("hq_str_s_" + item);
+                if (typeof (elements) != "undefined") {
+                    var eleArray = elements.split(",");
+                    func(item, eleArray[1], eleArray[3]);
+                }
+            });
+        }
+    });
+}
+
+function getStockStr(codes) {
+    var codesArray = new Array();
+    codes.forEach(function (item, index) {
+        codesArray.push("s_" + item);
+    });
+    return codesArray.join();
+}
